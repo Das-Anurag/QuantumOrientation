20b0e0bc3931699dff541c342da64aa1c733f079
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-08 04:41:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,33 +3,14 @@ import * as THREE from "three";
 import { OrbitControls }
     from "three/addons/controls/OrbitControls.js";
 
-import { Vector3 }
-    from "./functions.js";
 
-
-// ----------------------------------------
-// 1. Test our mathematical Vector3 class
-// ----------------------------------------
-
-const Phi = new Vector3(1, 2, 3);
-
-console.log("Phi =", Phi.show());
-console.log("Length of Phi =", Phi.length());
-
-
-// ----------------------------------------
-// 2. Create the Three.js Scene
-// ----------------------------------------
-
+// Create Scene
 const scene = new THREE.Scene();
 
 scene.background = new THREE.Color(0x87CEEB);
 
 
-// ----------------------------------------
-// 3. Create the Camera
-// ----------------------------------------
-
+// Create Camera
 const camera = new THREE.PerspectiveCamera(
     60,
     window.innerWidth / window.innerHeight,
@@ -38,14 +19,10 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.set(10, 8, 10);
-
 camera.lookAt(0, 0, 0);
 
 
-// ----------------------------------------
-// 4. Create the Renderer
-// ----------------------------------------
-
+// Create Renderer
 const renderer = new THREE.WebGLRenderer({
     antialias: true
 });
@@ -55,19 +32,12 @@ renderer.setSize(
     window.innerHeight
 );
 
-renderer.setPixelRatio(
-    Math.min(window.devicePixelRatio, 2)
-);
-
 document.body.appendChild(
     renderer.domElement
 );
 
 
-// ----------------------------------------
-// 5. OrbitControls
-// ----------------------------------------
-
+// OrbitControls
 const controls = new OrbitControls(
     camera,
     renderer.domElement
@@ -76,34 +46,23 @@ const controls = new OrbitControls(
 controls.enableDamping = true;
 
 
-// ----------------------------------------
-// 6. Coordinate Axes
-// ----------------------------------------
-
+// Axes
 const axesHelper = new THREE.AxesHelper(5);
 
 scene.add(axesHelper);
 
 
-// ----------------------------------------
-// 7. Lighting
-// ----------------------------------------
+// Animation
+function animate() {
 
-const directionalLight =
-    new THREE.DirectionalLight(
-        0xffffff,
-        1
-    );
+    requestAnimationFrame(animate);
 
-directionalLight.position.set(
-    5,
-    5,
-    5
-);
+    controls.update();
 
-scene.add(directionalLight);
+    renderer.render(scene, camera);
+}
 
-
+animate();
 const ambientLight =
     new THREE.AmbientLight(
         0xffffff,
