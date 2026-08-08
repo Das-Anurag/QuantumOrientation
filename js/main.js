ea6391b55d7d968840b6c8477d0cd518b1b92520
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-08 05:30:23 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 import * as THREE from "three";
 
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(400, 300);
+
+document.body.appendChild(renderer.domElement);
+
 document.body.innerHTML +=
-    "<p>Three.js loaded successfully!</p>";
+    "<p>Renderer created successfully!</p>";
