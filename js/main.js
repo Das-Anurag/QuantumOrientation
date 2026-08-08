8169354a76adcab7fdab60c5288b4ebb0a86537d
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-08 21:56:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
 import * as THREE from "three";
 
+// ----------------------------------------
+// 1. Scene
+// ----------------------------------------
+
+const scene = new THREE.Scene();
+
+scene.background = new THREE.Color(0x87CEEB);
+
+
+// ----------------------------------------
+// 2. Camera
+// ----------------------------------------
+
+const camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+camera.position.set(5, 5, 5);
+
+camera.lookAt(0, 0, 0);
+
+
+// ----------------------------------------
+// 3. Renderer
+// ----------------------------------------
+
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(400, 300);
+renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+);
 
-renderer.setClearColor(0x87CEEB);
+document.body.appendChild(
+    renderer.domElement
+);
 
-document.body.appendChild(renderer.domElement);
 
-renderer.clear();
+// ----------------------------------------
+// 4. Render
+// ----------------------------------------
+
+renderer.render(
+    scene,
+    camera
+);
