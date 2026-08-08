e080cfaf358fa4959658a74daa4bf3096934acb4
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-08 05:41:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 import * as THREE from "three";
 
+const scene = new THREE.Scene();
+
+scene.background = new THREE.Color(0x87CEEB);
+
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(400, 300);
 
 document.body.appendChild(renderer.domElement);
 
+renderer.render(scene, new THREE.Camera());
+
 document.body.innerHTML +=
-    "<p>Renderer created successfully!</p>";
+    "<p>Scene rendered successfully!</p>";
