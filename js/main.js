66949a6c887e3e971acc80845209de43a7010058
Das-Anurag <rmicrosoft365@gmail.com>
2026-08-16 22:18:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ const effectivePhiHitBoxes = [];
 // 9. CREATE THE 3 × 3 × 3 FIELD
 // ==================================================
 
-const gridValues = [-2, 0, 2];
+const gridValues = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1 0,1,2,3,4,5,6,7,8,9,10];
 
 for (let x of gridValues) {
 
